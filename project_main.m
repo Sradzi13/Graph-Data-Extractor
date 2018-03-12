89a99245b86c478c8ade1514dc5b29cdfca10f49
sradzi13 <sradzi13@stanford.edu>
2018-03-11 19:43:04 -0700

    fixed bug in detect_labels. still have to finish logarithmic interpolation

--- project_main.m
+++ project_main.m
@@ -1,4 +1,5 @@
 clear all; close all;
+
 imgFile = 'data/Template20_log_multiple_corner.jpg';
 I = imread(imgFile);
 figure, imshow(I), title('original image');
@@ -8,7 +9,7 @@ image = rgb2gray(I);
 
 % ask user for axis type
 ylinear = input('Enter 1 for linear y axis, 0 for log axis: ');
-% {
+%{
 % ask user for axis values
 x1 = input('Enter minimum x value: ');
 x2 = input('Enter maximum x value: ');
@@ -16,7 +17,7 @@ xvalues = [x1 x2];
 y1 = input('Enter minimum y value: ');
 y2 = input('Enter maximum y value: ');
 yvalues = [y1 y2];
-% }
+%}
 
 %% Detect Axes
 [origin,xaxis,yaxis] = detect_axis(image);
