32158d00e6c0e4bd7f5a7b73d4f54cdea77c171c
Deepti Mahajan <DMahajan@users.noreply.github.com>
2018-03-16 22:35:19 -0700

    cleaned preprocess code

--- preprocess.m
+++ preprocess.m
@@ -2,9 +2,7 @@ function imgFile_out = preprocess(imgFile)
     img = imread(imgFile);
     img_gray = rgb2gray(img);
     
-    % remove noise
-    
-    % rectify axes
+    % straighten axes
     img_edge = edge(img_gray, 'Canny');
     [H,T,R] = hough(img_edge);
     peaks = houghpeaks(H,4); %4 bounding lines of graph
@@ -18,22 +16,10 @@ function imgFile_out = preprocess(imgFile)
             break
         end
     end
-    %{
-    mask1 = thetas==theta1;
-    if sum(mask1(:))>1
-        rho1 = min(rhos(mask1));
-    end
-    mask2 = thetas==theta2;
-    if sum(mask2(:))>1
-        rho2 = min(rhos(mask2));
-    end
     
-    houghlines(img_edge,
-    %}
     theta = min(abs(theta1),abs(theta2));
     img_out = imrotate(img,theta);
-    %figure; imshow(img_out);
     [~,file,ext] = fileparts(imgFile);
-    imgFile_out = strcat(file,'_out.jpg');
+    imgFile_out = strcat(file,'_out.',ext);
     imwrite(img_out,imgFile_out);
 end
